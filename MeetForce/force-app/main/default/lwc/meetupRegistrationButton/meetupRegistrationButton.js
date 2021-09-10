87df3e01a30c79bfcce8e07de5fbe4bed10bebc0
Hydropanda <droniton@gmail.com>
2021-09-10 18:31:06 -0400

    Meetup Registration Button Implementation

---
+++ MeetForce/force-app/main/default/lwc/meetupRegistrationButton/meetupRegistrationButton.js
@@ -0,0 +1,57 @@
+import { LightningElement, api, wire, track } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+
+import registrationIsOpen from '@salesforce/apex/MeetupRegistrationHelper.registrationIsOpen';
+import REG_CODE_FIELD from '@salesforce/schema/Meetup__c.RegistrationCode__c';
+
+export default class MeetupRegistrationButton extends NavigationMixin(LightningElement) {
+    @api recordId;
+
+    regCode;
+    url;
+    registrationAvailability;
+    @track buttonActive;
+
+    @wire(registrationIsOpen, { recordId: '$recordId' }) 
+    wiredResult ({error, data}) {
+        if (data) {
+            if (data === 'This Meetup is open.')
+                this.buttonActive = true;
+            else {
+                this.registrationAvailability = data;
+                this.buttonActive = false;
+            }
+        } else if (error) {
+            this.buttonActive = false;
+            this.registrationAvailability = error.body;
+        }
+    };
+
+    @wire(getRecord, { recordId: 'recordId', fields: [REG_CODE_FIELD]})
+    wiredRecord ({ error, data }) {
+        if (data) {
+            this.regCode = getFieldValue(data, REG_CODE_FIELD);
+        } else if (error) {
+            registrationAvailability = 'Error retrieving record.';
+            this.buttonActive = false;
+        }
+    }
+    navigateToRegistration(evt) {
+
+        evt.preventDefault();
+        evt.stopPropogation();
+
+        regPageRef = {
+            type: 'standard__component',
+            attributes: {
+                componentName: 'c__meetupRegistrationForm'
+            },
+            state: {
+                regCode: this.regCode
+            }
+        }
+
+        this[NavigationMixin.Navigate](regPageRef);
+    }
+}
